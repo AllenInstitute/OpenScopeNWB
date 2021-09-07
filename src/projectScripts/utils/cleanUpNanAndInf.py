3bd3dba872fab3e56c2799e2999762c24e1650a7
Ahad-Allen <87045911+Ahad-Allen@users.noreply.github.com>
2021-09-07 11:24:22 -0700

    Update utils for Linting

--- src/projectScripts/utils/cleanUpNanAndInf.py
+++ src/projectScripts/utils/cleanUpNanAndInf.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def cleanUpNanAndInf(value):
     if np.isnan(value) or np.isinf(value):
         return -1
--- src/projectScripts/utils/parseParameterJSON.py
+++ src/projectScripts/utils/parseParameterJSON.py
@@ -1,22 +1,26 @@
-import numpy as np
-
 def getSessionID(parameterJSON):
     "Placeholder"
+
 
 def getModules(parameterJSON):
     "Placeholder"
 
+
 def getDirectories(parameterJSON):
     "Placeholder"
+
 
 def getOutputDirectory(parameterJSON):
     "Placeholder"
 
+
 def getJSONDirectory(parameterJSON):
     "Placeholder"
 
+
 def getLimsPath(parameterJSON):
     "Placeholder"
+
 
 def getSavePath(parameterJSON):
     "Placeholder"
