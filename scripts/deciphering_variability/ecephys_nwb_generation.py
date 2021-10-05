b26d0bc4534cf47f94e0ca4239479cb4c469c958
Ahad-Allen <87045911+Ahad-Allen@users.noreply.github.com>
2021-10-05 14:09:42 -0700

    Updating to use module_params structure in place of session

--- scripts/deciphering_variability/ecephys_nwb_generation.py
+++ scripts/deciphering_variability/ecephys_nwb_generation.py
@@ -11,6 +11,7 @@ warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 project_parameter_json = "path to project file"
 project_params = ppp.parse_json(project_parameter_json)
 sessions = ppp.get_session_ids(project_params)
+session_param_list = ppp.generate_all_session_params(project_params)
 modules = ppp.get_modules(project_params)
 session_modules, probe_modules = ppp.get_module_types(project_params)
 
@@ -28,13 +29,14 @@ probe_modules = ['allensdk.brain_observatory.ecephys.stimulus_table',
                  'allensdk.brain_observatory.extract_running_speed',
                  'allensdk.brain_observatory.ecephys.write_nwb']
 '''
-for session in sessions:
+for session_params in session_param_list:
     # for module in session_modules
     # for module in probe_modules
     # session_params = ppp.generate_session_params(project_params,
     #     
     #                                                session)
-    
+    '''
+    Test session_params
     session_params = {
         'session_id': '725254892',
         'base_directory': "C:\\Users\\ahad.bawany\\Documents\\ephys_session_725254892_demo\\ephys_session_725254892_demo",
@@ -44,16 +46,19 @@ for session in sessions:
         'probe_dict_list': [],
         'trim': False
     }
+    '''
+    session = session_params["session_id"]
     probes = session_params['probes']
     for module in modules:
         json_directory = ppp.get_input_json_directory(project_params)
         input_json = os.path.join(json_directory, session + '-' + module
-                                    + '-input.json')
+                                  + '-input.json')
         output_json = os.path.join(json_directory, session + '-' + module
-                                    + '-output.json')
+                                   + '-output.json')
         if module in session_modules:
-            session_params = osnjson.create_module_input(
-                module, session_params, input_json)
+            module_params = session_params
+            module_params = osnjson.create_module_input(
+                module, module_params, input_json)
 
             command_string = ["python", "-W", "ignore", "-m", module,
                               "--input_json", input_json,
@@ -61,10 +66,12 @@ for session in sessions:
             logging.debug("Starting Session Level Module")
             subprocess.check_call(command_string)
             logging.debug("Finished Session Level Module")
-        elif module in probe_modules: 
+        elif module in probe_modules:
             for probe in probes:
-                session_params = osnjson.create_module_input(
-                    module, session_params, input_json)
+                module_params = session_params
+                module_params['current_probe'] = probe
+                module_params = osnjson.create_module_input(
+                    module, module_params, input_json)
                 command_string = ["python", "-W", "ignore", "-m", module,
                                   "--input_json", input_json,
                                   "--output_json", output_json]
