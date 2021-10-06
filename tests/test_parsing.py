6daf9795ecb357b5a7f0fd074fd9e3fa6e722f76
Ahad-Allen <87045911+Ahad-Allen@users.noreply.github.com>
2021-10-06 09:58:41 -0700

    Adding testing for all basic  parsing functions

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -25,7 +25,6 @@ def json_prep(tmpdir):
         "session_dir": "session_dir",
         "trim_discontiguous_frame_times": False,
         "last_unit_id": 1,
-        "string": "Hello World"
     }
     json_path = os.path.join(tmpdir, "test.json")
     with open(json_path, 'w') as json_file:
@@ -36,4 +35,20 @@ def json_prep(tmpdir):
 def test_parsing(json_prep, tmpdir):
     test_dict = ppp.parse_json(json_prep)
     test_probes = ppp.get_probes(test_dict)
+    test_ids = ppp.get_session_ids(test_dict)
+    test_modules = ppp.get_modules(test_dict)
+    test_input = ppp.get_input_json_directory(test_dict)
+    test_output = ppp.get_output_json_directory(test_dict)
+    test_lims = ppp.get_lims_path(test_dict)
+    test_nwb = ppp.get_output_nwb_path(test_dict)
+    test_sess_dir = ppp.get_session_dir(test_dict)
+    test_trim = ppp.get_trim(test_dict)
     assert test_probes == ["probeA"]
+    assert test_ids == ["session"]
+    assert test_modules == ["module"]
+    assert test_input == "input_path"
+    assert test_output == "output_path"
+    assert test_lims == ["test"]
+    assert test_nwb == "nwb_path"
+    assert test_sess_dir == "session_dir"
+    assert test_trim is False
