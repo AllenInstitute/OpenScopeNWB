4d05426687ea539796f4f30934dfedb03263835a
Ahad-Allen <87045911+Ahad-Allen@users.noreply.github.com>
2021-10-12 14:49:41 -0700

    Adding a test to check a file against hashed output files

---
+++ tests/outputs.py
@@ -0,0 +1,78 @@
+import os
+import subprocess
+import warnings
+import logging
+import hashlib
+import sys
+
+import openscopenwb.create_module_input_json as osnjson
+from openscopenwb.utils import script_functions as sf
+from openscopenwb.utils import parse_project_parameters as ppp
+
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+
+logging.basicConfig(filename="std.log",
+                    format='%(asctime)s %(message)s',
+                    level=logging.DEBUG,
+                    filemode='a')
+
+
+dir = os.path.dirname(__file__)
+project_parameter_json = os.path.join(dir, "project_json",
+                                      "test_project_parameter_json.json")
+project_params = ppp.parse_json(project_parameter_json)
+session_param_list = ppp.generate_all_session_params(project_params)
+modules = ppp.get_modules(project_params)
+session_modules, probe_modules = ppp.get_module_types(project_params)
+
+for session_params in session_param_list:
+    session = session_params["session_id"]
+    probes = session_params['probes']
+    for module in modules:
+        json_directory = ppp.get_input_json_directory(project_params)
+        input_json = os.path.join(json_directory, session + '-' + module
+                                  + '-input.json')
+        output_json = os.path.join(json_directory, session + '-' + module
+                                   + '-output.json')
+        if module in session_modules:
+            module_params = session_params
+            module_params = osnjson.create_module_input(
+                module, module_params, input_json)
+
+            command_string = sf.generate_module_cmd(
+                                module,
+                                input_json,
+                                output_json
+            )
+            logging.debug("Starting Session Level Module: " + module)
+            subprocess.check_call(command_string)
+            logging.debug("Finished Session Level Module: " + module)
+        elif module in probe_modules:
+            for probe in probes:
+                module_params = session_params
+                module_params['current_probe'] = probe
+                module_params['id'] = probes.index(probe)
+                module_params = osnjson.create_module_input(
+                    module, module_params, input_json)
+                command_string = sf.generate_module_cmd(
+                                    module,
+                                    input_json,
+                                    output_json
+                )
+                logging.debug("Starting Probe Level Module:: " + module)
+                subprocess.check_call(command_string)
+                logging.debug("Finished Probe Level Module: " + module)
+
+
+def sha256sum(file):
+    hasher = hashlib.sha256()
+    bytes = bytearray(128*1024)
+    mem_view = memoryview(bytes)
+    with open(file, 'rb', buffering=0) as file_name:
+        for n in iter(lambda: file_name.readinto(mem_view), 0):
+            hasher.update(mem_view[:n])
+    return hasher.hexdigest()
+
+
+def check_hash(file):
+    return file == sha256sum(sys.argv[1])
