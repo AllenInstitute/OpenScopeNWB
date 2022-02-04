476c26f1b49bf9b4ad7539fe532b65d6fdb1614d
Ahad-Allen <87045911+Ahad-Allen@users.noreply.github.com>
2022-02-04 08:22:37 -0800

    adding slurm functionality to scripts

---
+++ scripts/deciphering_variability/slurm_test.py
@@ -0,0 +1,25 @@
+from simple_slurm import Slurm
+import os
+
+conda_environment = 'slurm_test'
+
+python_path = os.path.join(
+    os.path.expanduser("~"),
+    'anaconda3',
+    'envs',
+    conda_environment,
+    'bin',
+    'python'
+)
+slurm = Slurm(
+    array=range(3, 4),
+    cpus_per_task=15,
+    job_name='openscope_test',
+    dependency=dict(after=65541, afterok=34987),
+    mem='8gb',
+    output=f'{Slurm.JOB_ARRAY_MASTER_ID}_{Slurm.JOB_ARRAY_ID}.out'
+)
+
+slurm.sbatch(r'/allen/programs/mindscope/workgroups/openscope/ahad/' +
+             r'test_script/OpenScopeNWB-feature-ephys_write_stim/' +
+             r'scripts/deciphering_variability/ecephys_nwb_generation.py')
