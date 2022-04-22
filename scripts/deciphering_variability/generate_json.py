e6c716833b87aa07b40fea274d975599a3eb94ac
Ahad-Allen <87045911+Ahad-Allen@users.noreply.github.com>
2022-04-22 10:23:20 -0700

    Ophys json gen

--- scripts/deciphering_variability/generate_json.py
+++ scripts/deciphering_variability/generate_json.py
@@ -1,3 +1,4 @@
+import json
 from openscopenwb.utils import postgres_functions as postgres
 import os
 from glob import glob
@@ -13,4 +14,18 @@ def get_probes(path):
     for probe_idx in probes:
         existence_list = glob(os.path.join(path, '*' + probe_idx + '*_sorted'))
         print(existence_list)
-        
+
+
+def generate_ophys_json(session_id):
+    json_data = {}
+    for experiment in postgres.get_sess_experiments(session_id):   
+        json_data['Session_ids'] = {experiment : session_id}
+    json_out = json.dumps(json_data)
+    return json_out
+
+
+
+
+print(postgres.get_sess_experiments(1137276124))
+print(postgres.get_sess_probes(829720705))
+print(generate_ophys_json(1137276124))
