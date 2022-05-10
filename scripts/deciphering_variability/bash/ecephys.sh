072445c99a39e17e63b3cc9317d8908e6f1460f8
Jerome Lecoq <jeromel@alleninstitute.org>
2022-05-10 15:36:45 -0700

    reorganize repo

--- scripts/deciphering_variability/bash/ecephys.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-ssh ahad.bawany@hpc-login "/allen/programs/mindscope/workgroups/openscope/ahad/test_cron/OpenScopeNWB-feature-firebase_testing/scripts/deciphering_variability/bash/ece_job.sh" 
-exit 1
--- scripts/slurm_ephys_job.py
+++ scripts/slurm_ephys_job.py
@@ -1,5 +1,4 @@
 import slurm_job
 import slurm_temp
-import sys
 
 slurm_job.generate_ephys_nwb(slurm_temp.get_text())
