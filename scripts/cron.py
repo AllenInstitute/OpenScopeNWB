0933962462874fbeaac91e11c5ba0ade3c7d0a21
Ahad Bawany <ahad.bawany@ibs-ahadb-vm1.corp.alleninstitute.org>
2022-05-12 16:35:42 -0700

    Adding temporary changes to handle stim_path errors

--- scripts/cron.py
+++ scripts/cron.py
@@ -12,6 +12,7 @@ dir = os.path.dirname(__file__)
 cred_json = fb.get_creds()
 
 #ephys_list = fb.update_ephys_statuses()
-session_id = 762602078
+session_id = 1172129291
+#session_id = 762602078
 cmd = './bash/ecephys.sh ' + "-s " + str(session_id)
 subprocess.call(shlex.split(cmd))
