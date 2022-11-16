66784314223d77643194bd789a01be692be97262
Ahad Bawany <ahad.bawany@ibs-ahadb-vm1.corp.alleninstitute.org>
2022-11-16 13:50:55 -0800

    Adding dandi loc to firebase location for website

--- src/openscopenwb/utils/postgres_functions.py
+++ src/openscopenwb/utils/postgres_functions.py
@@ -458,6 +458,7 @@ def get_e_sess_info(session_id):
     meta_dict['type'] = 'Ecephys'
     meta_dict['status'] = {'status': 'Not Converted'}
     meta_dict['notes'] = 'none'
+    meta_dict['dandi'] = 'Not Yet Uploaded'
     try:
         meta_dict['probes'] = get_sess_probes(session_id)
     except Exception:
@@ -486,7 +487,8 @@ def get_o_sess_info(session_id):
         os.stimulus_name,
         os.date_of_acquisition,
         os.operator_id,
-    os.imaging_depth_id
+        os.imaging_depth_id,
+        os.workflow_state
     FROM ophys_sessions os
     WHERE os.id = {}
     """
@@ -518,6 +520,8 @@ def get_o_sess_info(session_id):
     meta_dict['operator'] = info_list[6]
     meta_dict['equip'] = info_list[3]
     meta_dict['id'] = info_list[0]
+    meta_dict['workflow'] = info_list[8]
+    meta_dict['dandi'] = 'Not Yet Uploaded'
     meta_dict['path'] = get_o_sess_directory(session_id)[0]
     meta_dict['type'] = 'Ophys'
     meta_dict['experiments'] = get_sess_experiments(session_id)
