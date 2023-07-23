1210073acafa5c9e4eee71fa50db962584fdf944
Ahad-Allen <87045911+Ahad-Allen@users.noreply.github.com>
2023-07-23 15:39:33 -0700

    Adding firebase function for login for clean_up_functions.py

--- src/openscopenwb/utils/clean_up_functions.py
+++ src/openscopenwb/utils/clean_up_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import json
 
 
-def get_creds():
+def get_dandi_creds():
     """Gets the dandi credentials locally
 
     Parameters
@@ -18,6 +18,26 @@ def get_creds():
         r'/allen/programs/mindscope/workgroups/openscope/ahad/test_cron/OpenScopeNWB-feature-firebase_testing/src/openscopenwb/utils/.cred/dandi.json')
     cred_json = json.load(cred_file)
     return cred_json['api_key']
+
+
+def get_firebase_creds():
+    """Gets the firebase credentials locally
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    cred_json: str
+    The credentials for firebase
+    """
+    dir = os.path.dirname(__file__)
+    credential_file = glob.glob(os.path.join(dir, '.cred',
+                                'firebase_backend_credentials.json'))
+    cred_json = credential_file[0]
+    return cred_json
+
 
 def clean_up_nan_and_inf(value):
     """ Replaces Nan and inf values with -1
