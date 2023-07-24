3cd9e82f338b91ad0f489df31353659836f7db59
Ahad-Allen <87045911+Ahad-Allen@users.noreply.github.com>
2023-07-23 17:21:25 -0700

    Create slurm_temp_job.py

---
+++ src/openscopenwb/utils/slurm_utils/slurm_temp_job.py
@@ -0,0 +1,13 @@
+import slurm_job
+import sys
+
+if __name__ == '__main__':
+    '''Adds image templates to an ephys nwb
+
+    Parameters
+    ----------
+
+    Returns
+    -------
+    '''
+    slurm_job.add_temp_to_nwb()
